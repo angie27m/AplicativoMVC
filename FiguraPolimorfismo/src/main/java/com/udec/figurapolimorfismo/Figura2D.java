dc24506746d90f7434ddd244874e600f8056c933
Manrique <angie27manrique@gmail.com>
2019-09-01 16:44:12 -0500

    Esqueleto del proyecto

---
+++ FiguraPolimorfismo/src/main/java/com/udec/figurapolimorfismo/Figura2D.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.udec.figurapolimorfismo;
+
+/**
+ *
+ * @author angie
+ */
+public abstract class Figura2D {
+    
+}
